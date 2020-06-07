c99e3cfa0c212d9805d7382c1517684c45b8f10a
liu3594211 <1269988493@qq.com>
2020-06-07 22:40:57 +0800

    liu

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page666</h1>
+    <div>12</div>
     git pushgit remote add origin git@github.com:liu3594211/-.git发发发
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    123vssv
+    123可以提交了嘛
   </div>
 </template>
 
